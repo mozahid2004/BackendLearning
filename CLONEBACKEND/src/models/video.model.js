9d5ab86220fd159ef63740f37dfa87bcd7c587cc
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-02-28 09:20:19 +0530

    Add user and VEDIO model

---
+++ CLONEBACKEND/src/models/video.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+  videoFile: {
+    type: String,//cloudnary url
+    required: true
+  },
+  thumbnail: {
+    type: String,//cloudnary url
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  discription: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,//cloudnary url
+    default: 0
+  },
+  isPublished: {
+    type: Boolean,
+    default: true
+  },
+  ownwer: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, { timestamps: true })
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Vidio", videoSchema)
