b846b97810e073ca44c6d2fb142598441a21be49
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-02-28 08:08:13 +0530

    Utils disscussiion

--- CLONEBACKEND/src/db/index.js
+++ CLONEBACKEND/src/db/index.js
@@ -4,9 +4,8 @@ import { DB_NAME} from "../constants.js"
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
     console.log(`\n MONGODB connected !! DB HOST : ${connectionInstance}`);
-    console.log(connectionInstance);
+    // console.log(connectionInstance);cl
 
   } catch (error) {
     console.log("MONGODB connection : ", error); 
